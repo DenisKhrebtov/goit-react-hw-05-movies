de7f8db948e1f379bb50b29be0496d115d9c525f
DenisKhrebtov <khrebtovdenis@gmail.com>
2022-11-12 23:31:08 +0200

    *

--- src/api/api.js
+++ src/api/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 axios.defaults.baseURL = 'https://api.themoviedb.org/3/';
 const API_KEY = '572822f6c91e4ecf30bc82c47474f59a';
 
-export async function fetchTrendFilms(query, page) {
+export async function fetchTrendFilms() {
   try {
     const response = await axios.get(`/trending/movie/day?api_key=${API_KEY}`);
     return response.data;
