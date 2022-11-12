1e76af0ff2aea400a10e00d6588f99db7d1cb736
DenisKhrebtov <khrebtovdenis@gmail.com>
2022-11-12 22:04:57 +0200

    started hw

---
+++ src/api/api.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://api.themoviedb.org/3/';
+const API_KEY = '572822f6c91e4ecf30bc82c47474f59a';
+
+export async function fetchTrendFilms(query, page) {
+  try {
+    const response = await axios.get(`/trending/movie/day?api_key=${API_KEY}`);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
+}
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,28 @@
+import { Routes, Route, Link } from 'react-router-dom';
+
+import { Home } from '../pages/Home';
+import { Movies } from '../pages/Movies';
+import { NotFound } from '../pages/NotFound';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <nav>
+        <Link to="/" end>
+          Home
+        </Link>
+        <Link to="/movies">Movies</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<Movies />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </div>
   );
 };
+
+// Ключ API (v3 auth)
+// 572822f6c91e4ecf30bc82c47474f59a
+// Пример API-запроса
+// https://api.themoviedb.org/3/movie/550?api_key=572822f6c91e4ecf30bc82c47474f59a
